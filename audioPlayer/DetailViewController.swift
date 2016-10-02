5219122ee092e18ece5fe68704314710d7860dd3
marcodelangel <marco_del_angel@hotmail.com>
2016-10-01 19:06:22 -0500

    2do commit detail completo

--- audioPlayer/DetailViewController.swift
+++ audioPlayer/DetailViewController.swift
@@ -7,19 +7,33 @@
 //
 
 import UIKit
+import AVFoundation
 
 class DetailViewController: UIViewController {
 
+    private var reproductor : AVAudioPlayer!
+    
     @IBOutlet weak var coverImage: UIImageView!
     var cover : UIImage = UIImage(imageLiteral: "noCoverImage.jpg")
-    
     @IBOutlet weak var songName: UILabel!
+    var tituloDeLaCancion : String = "No song selected"
+    var songURL = NSURL()
+    @IBOutlet weak var volumeSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         coverImage.image = cover
+        songName.text = tituloDeLaCancion
+        
+        do{
+            try reproductor =  AVAudioPlayer(contentsOfURL: songURL)
+        }catch{
+            print("Error en la reproducción")
+        }
+        
+        reproductor.play()
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,7 +41,34 @@ class DetailViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func play() {
+        if !reproductor.playing{
+            reproductor.play()
+        }
+    }
 
+    @IBAction func pause() {
+        if reproductor.playing{
+           reproductor.pause()
+        }
+    }
+    
+     @IBAction func stop() {
+        if reproductor.playing{
+            reproductor.pause()
+            reproductor.currentTime = 0.0
+        }
+        if !reproductor.playing{
+            reproductor.currentTime = 0.0
+        }
+     }
+   
+    @IBAction func volumen() {
+        
+        let volume = volumeSlider.value
+        reproductor.volume = volume
+        
+    }
     /*
     // MARK: - Navigation
 
@@ -35,7 +76,7 @@ class DetailViewController: UIViewController {
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
-    }
-    */
+    }*/
+
 
 }
